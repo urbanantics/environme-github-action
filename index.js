20420f2f5a8fdb567c2ee6df1eb0b5c21116b64a
Reuben <reubenhutton@hotmail.com>
2020-02-10 11:05:16 +0000

    Update action test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
   console.log(`Path ${path}!`);
 
   const branchName  = process.env.GITHUB_HEAD_REF;
-  console.log(`branchName: ${branchName}`);
+  console.log(`branchName: ${branchName}!!`);
 
   // `targetEnvironment` input defined in action metadata file
   const targetEnvironment = core.getInput('targetEnvironment');
